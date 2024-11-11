3fca69bf448e77925dbc20bb0fd5c0cc9e218998
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-11-11 12:32:01 +0300

    installed related dependencies and initialized the app

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+# from model import *
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farmart.db'
+app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return 'Hello Dearest Farmer'
+
+# @app.route('/farmers')
