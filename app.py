0110c7c4058e52a53342712bf162d1fba897c688
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-11-13 19:53:33 +0300

    applied jwt extended and created .env file to store secret key and sqlite configurations

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from models import *
 from config.database import db
 from flask_jwt_extended import jwt_required, JWTManager
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farmart.db'
-app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+# app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config ['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+app.config ['JWT_TOKEN_LOCATION'] = ['headers']
+
+jwt = JWTManager(app)
 
 migrate = Migrate(app, db)
 
