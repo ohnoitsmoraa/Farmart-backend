a13977fef4e3ffee6b6ca0142b6b7d4f9969c1fd
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-11-15 09:55:22 +0300

    Added password attr & password hashing  to user/farmer models

--- models/farmer.py
+++ models/farmer.py
@@ -1,4 +1,5 @@
 from config.database import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Farmer(db.Model):
     __tablename__ = "farmers"
@@ -8,8 +9,15 @@ class Farmer(db.Model):
     email = db.Column(db.String, unique=True, nullable=False)
     farm_name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=True)
+    password_hash = db.Column(db.String(128), nullable=False)
 
     animals = db.relationship("Animal", back_populates="farmer")
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def to_dict(self):
         return{
