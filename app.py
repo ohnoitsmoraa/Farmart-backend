5770b498afe55f881a5903d838ae74841f1028a2
Nelly <nmatu308@gmail.com>
2024-11-19 13:18:17 +0300

    update the url

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from flask_restful import Api, Resource
 load_dotenv()
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config ['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+app.config ['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config ['JWT_TOKEN_LOCATION'] = ['headers']
 
 jwt = JWTManager(app)
