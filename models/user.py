76d8f5d6156eacea5e10343286a405cce36b73a1
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-11-20 12:46:34 +0300

    in users model added password generation and email validation

--- models/user.py
+++ models/user.py
@@ -1,4 +1,6 @@
 from config.database import db
+from sqlalchemy.orm import validates
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = "users"
@@ -6,6 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String(50))
 
     orders = db.relationship("Order", back_populates="user")
     cart_items = db.relationship("Cart", back_populates="user")
@@ -16,6 +19,18 @@ class User(db.Model):
             "name": self.name,
             "email": self.email,
         }
+    
+    @validates ('email')
+    def validate_email (self, key, value):
+        if '@' not in value:
+            raise ValueError('Invalid email address')
+        return value
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
     def __repr__(self):
         return f"<User(id={self.id}, name='{self.name}', email='{self.email}')>"
